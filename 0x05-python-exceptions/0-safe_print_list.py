fc407165556e8c95794c05e63b1c8c7961aa2726
silasotis <silasogot@gmail.com>
2023-06-26 12:43:38 -0700

    exeption

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    num = 0
+    for n in range(x):
+        try:
+            print("{}".format(my_list[n]), end="")
+            num += 1
+     except IndexError:
+            break
+    print("")
+    return (num)
