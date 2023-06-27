115b1ee55d49e2b6bd01b6a348cf23009a7e3ce1
silasotis <silasogot@gmail.com>
2023-06-27 15:07:33 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
-"""Defines a square based on 0-square.py"""
-
 
 class Square:
-	    """The sqaure"""
 
     def __init__(self, size):
-        """initialise a square
-
-        Args:
-            size (int): Size of the square						
-        """
         self.__size = size
