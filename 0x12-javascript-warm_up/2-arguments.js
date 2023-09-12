e85a3b8b3d10906ed7d1f1f1e2928ccda4f6ac6e
silasotis <silasogot@gmail.com>
2023-09-12 04:32:28 -0700

    updates

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	  console.log('No argument');
-} else if (process.argv.length === 3) {
-	  console.log('Argument found');
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
 
